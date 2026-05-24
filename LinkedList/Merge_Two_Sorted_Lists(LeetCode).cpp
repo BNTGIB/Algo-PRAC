664745cf301e8036200c11001308bf6873089361
BNTGIB <nhattuanb@gmail.com>
2026-05-25 00:32:29 +0700

    Add Two Numbers

--- LinkedList/Merge_Two_Sorted_Lists(LeetCode).cpp
+++ LinkedList/Merge_Two_Sorted_Lists(LeetCode).cpp
@@ -28,9 +28,9 @@ Example 3:
 struct ListNode {
       int val;
       ListNode *next;
-      //ListNode() : val(0), next(nullptr) {}
-      //ListNode(int x) : val(x), next(nullptr) {}
-      //ListNode(int x, ListNode *next) : val(x), next(next) {}
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 class Solution {
